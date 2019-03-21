cd8db968a590026c896da7ba8f1837deb064e496
nanigasi <nanigasi.py@gmail.com>
2019-03-22 03:47:18 +0900

    類語のテキストを表示するプログラムに改良

--- gensim_sample.py
+++ gensim_sample.py
@@ -1,6 +1,8 @@
 import gensim
-model = gensim.models.KeyedVectors.load_word2vec_format("F:/vector/model.vec",binary=False)
-sim = model.most_similar(positive=["高専"])
-print(sim)
+model = gensim.models.KeyedVectors.load_word2vec_format(
+    "F:/vector/model.vec", binary=False)
+
+word = "高専"
+sim = model.most_similar(positive=[word])
 for _ in sim:
     print(_[0])

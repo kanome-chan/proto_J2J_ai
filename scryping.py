3edbfcacf5b4b7c433479792bb42e1241bbd92a3
nanigasi <nanigasi.py@gmail.com>
2019-03-23 06:41:55 +0900

    2,4,5をダウンロード完了

--- scryping.py
+++ scryping.py
@@ -14,6 +14,8 @@ def only_japanese(doc):
     return True
 
 #アルファベット抜きの単語帳
+
+#2,4,5だけ
 def make_text_from_html(word_difficulty,page_list):
     with open(word_difficulty+".txt","w",encoding="utf-8") as f:
         counter = 0
@@ -27,8 +29,11 @@ def make_text_from_html(word_difficulty,page_list):
                 if only_japanese(txt.strip()):
                     f.write(txt+"\n")
             print(word_difficulty+" : {0}/{1}".format(counter,len(page_list)))
-            time.sleep(0.5)
+            time.sleep(1)
     print("="*10)
 
 make_text_from_html("N5",[1005+i*3 for i in range(5)])
 make_text_from_html("N4",[990+i*3 for i in range(5)])
+N2list = [1028+i*3 for i in range(11)]
+N2list.extend([1062,1065,1068])
+make_text_from_html("N2",N2list)

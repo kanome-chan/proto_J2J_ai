6820c9cedc480dee9dbdf3dcad7d65aed5ebe3c1
nanigasi <nanigasi.py@gmail.com>
2019-04-18 08:55:32 +0900

    エラーページを書いた

--- render_demo.py
+++ render_demo.py
@@ -28,25 +28,28 @@ def render_j2j():
     if request.method == "GET":
         return render_template("demo.html")
     elif request.method == "POST":
-        target = request.form["target_word"]
-        sim = model.most_similar(positive=[target],topn=300)
-        sim2 = [_[0] for _ in sim] #単語のリスト
-        word_list6 = []
-        for n_list in difficulty_list:
-            for word in sim2:
-                if word in n_list:
-                    word_list6.append(word)
-                    if len(word_list6)>=6:
-                        break
-        n1,n2,n3,n4,n5,n6 = tuple(word_list6)
-        words_dic = {"main":target,
-                     "n1":n1,
-                     "n2":n2,
-                     "n3":n3,
-                     "n4":n4,
-                     "n5":n5,
-                     "n6":n6,}
-        return render_template("demo_2.html",words=words_dic)
-
+        try:
+            target = request.form["target_word"]
+            sim = model.most_similar(positive=[target],topn=300)
+            sim2 = [_[0] for _ in sim] #単語のリスト
+            word_list6 = []
+            for n_list in difficulty_list:
+                for word in sim2:
+                    if word in n_list:
+                        word_list6.append(word)
+                        if len(word_list6)==6:
+                            break
+            n1,n2,n3,n4,n5,n6 = tuple(word_list6)
+            words_dic = {"main":target,
+                         "n1":n1,
+                         "n2":n2,
+                         "n3":n3,
+                         "n4":n4,
+                         "n5":n5,
+                         "n6":n6,}
+            return render_template("demo_2.html",words=words_dic)
+        except:
+            return render_template("demo_error.html")
+    
 if __name__ == "__main__":
     app.run()

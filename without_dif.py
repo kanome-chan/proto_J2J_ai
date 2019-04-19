4c316efe84bd2fd484034f852e2f5883f54007ee
nanigasi <nanigasi.py@gmail.com>
2019-04-19 10:03:54 +0900

    難易度検査無しバージョンを作った

---
+++ without_dif.py
@@ -0,0 +1,39 @@
+from flask import*
+import gensim
+import pickle
+
+app = Flask(__name__)
+#読み込みにめっちゃ時間かかる
+print("W2V読み込み中...")
+try:
+    model = gensim.models.KeyedVectors.load_word2vec_format("F:/vector/model.vec", binary=False)
+except FileNotFoundError:
+    print("SSDセットした？")
+print("完了")
+
+
+@app.route("/",methods=["GET","POST"])
+def render_j2j():
+    if request.method == "GET":
+        return render_template("demo.html")
+    elif request.method == "POST":
+        try:
+            target = request.form["target_word"]
+            sim = model.most_similar(positive=[target],topn=100)
+            sim2 = [_[0] for _ in sim] #単語のリスト
+            word_list6 = sim2[:6]
+            print(word_list6)
+            n1,n2,n3,n4,n5,n6 = tuple(word_list6)
+            words_dic = {"main":target,
+                         "n1":n1,
+                         "n2":n2,
+                         "n3":n3,
+                         "n4":n4,
+                         "n5":n5,
+                         "n6":n6,}
+            return render_template("demo_2.html",words=words_dic)
+        except:
+            return render_template("demo_error.html")
+    
+if __name__ == "__main__":
+    app.run()

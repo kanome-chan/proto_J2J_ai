442e33fb7d17debf92633fefb5fa38d2637c8bd7
nanigasi <nanigasi.py@gmail.com>
2019-04-30 13:33:05 +0900

    Excelファイルを直列化するプログラムを関数にした

--- xlsx_to_pickle.py
+++ xlsx_to_pickle.py
@@ -1,19 +1,16 @@
-# In[]:
 import openpyxl
-book = openpyxl.load_workbook("data/D18-2018.7.24.xlsx")
-
-# 今回は作業者Aのシートを使う
-sheet = book["作業者A"]
-
-
-# In[]:
-dic = {}
-for row in sheet.rows:
-    dic[row[0].value] = tuple(row[2].value)
-del dic["Word"]
-# print(dic)
-
-# In[]:
 import pickle
-with open("pickle/worker_a.pickle","wb") as f:
-    pickle.dump(dic,f)
+
+def xlsx_to_pickle(worker_name):
+    book = openpyxl.load_workbook("data/D18-2018.7.24.xlsx")
+    sheet = book[worker_name]
+    word_dic = {}
+    for row in sheet.rows:
+        word_dic[row[0].value] = tuple(row[2].value)
+    del word_dic["Word"]
+    with open("pickle/{0}.pickle".format(worker_name),"wb") as f:
+        pickle.dump(word_dic,f)
+
+xlsx_to_pickle("作業者A")
+xlsx_to_pickle("作業者B")
+xlsx_to_pickle("作業者C")
